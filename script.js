2e3ba2aeaccc27bf1fc5959c79da61dfa8cfc57b
Sefa Gueven <sefagueven@gmx.de>
2026-09-03 13:35:42 +0200

    The template.js file has been added, as has the footer. The style.css file has been expanded.

--- script.js
+++ script.js
@@ -1,3 +1,22 @@
+const BASE_URL = "https://pokeapi.co/api/v2/pokemon/";
+const limit = 25;
+let offset = 0;
+let pokemonArray = [];
+let currentIndex = 0;
+let searchedPokemonArray = [];
+
+
+function init   () {
+const loadMoreButton = document.getElementById("load-more");
+loadMoreButton.style.display = "none";
+fetchData(offset);
+}
+function toggleLoadingSpinner(){
+    const loadingContainer = document.getElementById("loading-container");
+    loadingContainer.classList.toggle("hide-loading-spinner");
+}
+
+
 let allPkms= [{
 name: "pkm1",
 type: "grass"
