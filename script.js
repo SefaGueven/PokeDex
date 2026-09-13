aa3c6cd1aa83fb57bd5812933cd1f2a5b7062111
Sefa Güven <sefagueven003@gmail.com>
2026-09-13 17:33:42 +0200

    Basic structure of a Pokédex app with `offset`, `pokemonarray`, `currentIndex`, and `searchedPokemonArray`. In addition, `dom.references` are defined for use throughout the script.

--- script.js
+++ script.js
@@ -1,41 +1,15 @@
-const BASE_URL = "https://pokeapi.co/api/v2/pokemon/";
-const limit = 25;
+const BASE_URL = "https://pokeapi.co/api/v2/";
+const LIMIT = 28;
+//  Das sieht nach der Grundstruktur einer kleinen Pokédex-App aus – mit Paginierung (offset), 
+//  einer Gesamtliste (pokemonArray), einem aktuellen Anzeige-Index (currentIndex) und
+//  einem separaten Platz für Suchergebnisse (searchedPokemonArray).
 let offset = 0;
-let pokemonArray = [];
+let pokemonArray =[];
 let currentIndex = 0;
-let searchedPokemonArray = [];
+let searchedPokemonArray = null ;
 
-
-function init   () {
-const loadMoreButton = document.getElementById("load-more");
-loadMoreButton.style.display = "none";
-fetchData(offset);
-}
-function toggleLoadingSpinner(){
-    const loadingContainer = document.getElementById("loading-container");
-    loadingContainer.classList.toggle("hide-loading-spinner");
-}
-
-
-let allPkms= [{
-name: "pkm1",
-type: "grass"
-},
-]
-
-function renderNames() {
-    for (let i = 0; i < allPkms.length; i++) {
-    document.getElementById('list').innerHTML+=`<h1 class="bg_${allPkms[i].type}">${allPkms[i].name}</h1>`
-        
-    }
-    
-}
-function getColor(type) {
-    switch (type) {
-        case "grass":
-            return "#12541";
-        default:
-            return "#12541";
-    }
-    
-} 
+// DOM-Referenzen, die im ganzen Script wiederverwendet werden
+const myDialog =document.getElementById("pokemon-dialog");
+const loadMoreBtn = document.getElementById("loading-more-pokemon");
+const backToStart = document.getElementById("back-to-start-btn");
+const errorMessage = document.getElementById("error-no-match-found");
