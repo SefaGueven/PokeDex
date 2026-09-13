4f3625d69a1c49ac0cf872fb9f12e137fc04cd74
Sefa Güven <sefagueven003@gmail.com>
2026-09-13 18:36:55 +0200

    Retrieve Pokémon data from the API.

--- script.js
+++ script.js
@@ -14,8 +14,29 @@ const loadMoreBtn = document.getElementById("loading-more-pokemon");
 const backToStart = document.getElementById("back-to-start-btn");
 const errorMessage = document.getElementById("error-no-match-found");
 
-//Cleanup fdafür,dass die Seite wieder normal ist,sobald man den Dialogfenster schließt.
+//Cleanup dafür,dass die Seite wieder normal ist,sobald man den Dialogfenster schließt.
 myDialog.addEventListener("close",() => {
     myDialog.classList.remove("opened");
     document.body.classList.remove("no-scroll");
 });
+function init() {
+    loadMoreBtn.style.display ="none"; // Mehr Laden-button unsichtbar bisDaten da sind.
+    fetchData(offset);
+    
+}
+function toggleLoadingSpinner(){
+    document.getElementById("loading-container").classList.toggle("hide-loading-spinner");
+}
+async function fetchData(offset) {
+    try{
+        const response = await fetch(`${BASE_URL}pokemon?limit=${LIMIT}&offset=${offset}`);
+        if(!response.ok)throw new Error("There are problems loading the Pokémon! Please reload the page!");
+        const responseToJson = await response.json();
+        await getDetails(responseToJson);
+        }catch (error){
+            renderError(error.massage);
+        }
+    
+}
+
+
